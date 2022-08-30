ad6989da4e1566105e41171ab88aac3b4574cc42
liamjken <liamjken@gmail.com>
2022-08-30 16:18:22 -0400

    Update aim-admin.php
    adding AV Admin

--- includes/aim-admin.php
+++ includes/aim-admin.php
@@ -1,42 +1,45 @@
 <?php 
 
-add_action('admin_menu', 'aim_plugin_create_menu');
+add_action('admin_menu', 'av_plugin_create_menu');
 
   
-function aim_plugin_create_menu() {
+function av_plugin_create_menu() {
 
   //create new top-level menu
-  add_menu_page('AIM Plugin Settings', 'AIM Settings', 'administrator', __FILE__, 'aim_plugin_settings_page' , plugins_url('/img/icon.png', __DIR__), );
-  add_submenu_page(__FILE__, 'AIM Shortcode', 'AIM Shortcode', 'manage_options', __FILE__.'/custom', 'shortcode_aim');
+  add_menu_page('AV Plugin Settings', 'AV Settings', 'administrator', __FILE__, 'av_plugin_settings_page' , plugins_url('/img/av-icon.png', __DIR__), );
+  add_submenu_page(__FILE__, 'AV Shortcode', 'AV Shortcode', 'manage_options', __FILE__.'/custom', 'shortcode_av');
 
 
   //call register settings function
-  add_action( 'admin_init', 'register_aim_plugin_settings' );
+  add_action( 'admin_init', 'register_av_plugin_settings' );
 }
 
-function register_aim_plugin_settings() {
+function register_av_plugin_settings() {
   //register our settings
-  register_setting( 'aim-plugin-settings-group', 'dealer_city' );
-  register_setting( 'aim-plugin-settings-group', 'dealer_brand' );
-  register_setting( 'aim-plugin-settings-group', 'aim_dealer_id' );
-  register_setting( 'aim-plugin-settings-group', 'dealer_color' );
-  register_setting( 'aim-plugin-settings-group', 'dealer_secondary_color' );
-  register_setting( 'aim-plugin-settings-group', 'dealer_background_color' );
+  register_setting( 'av-plugin-settings-group', 'dealer_city' );
+  register_setting( 'av-plugin-settings-group', 'dealer_brand' );
+  register_setting( 'av-plugin-settings-group', 'aim_dealer_id' );
+  register_setting( 'av-plugin-settings-group', 'av_sdk_url' );
+  register_setting( 'av-plugin-settings-group', 'av_slack_url' );
+  register_setting( 'av-plugin-settings-group', 'av_import_order' );
+  register_setting( 'av-plugin-settings-group', 'dealer_color' );
+  register_setting( 'av-plugin-settings-group', 'dealer_secondary_color' );
+  register_setting( 'av-plugin-settings-group', 'dealer_background_color' );
 }
 
 
-//content within the AIM Settings tab
-function aim_plugin_settings_page() {
+//content within the av Settings tab
+function av_plugin_settings_page() {
 
   ob_start();
   ?> 
 
 <div class="wrap">
-<h1>AIM Settings</h1>
+<h1>av Settings</h1>
 
 <form method="post" action="options.php">
-  <?php settings_fields( 'aim-plugin-settings-group' ); ?>
-  <?php do_settings_sections( 'aim-plugin-settings-group' ); ?>
+  <?php settings_fields( 'av-plugin-settings-group' ); ?>
+  <?php do_settings_sections( 'av-plugin-settings-group' ); ?>
   <table class="form-table">
       <tr valign="top">
       <th scope="row">Dealership City</th>
@@ -51,6 +54,21 @@ function aim_plugin_settings_page() {
       <tr valign="top">
       <th scope="row">AIM dealer ID</th>
       <td><input type="text" name="aim_dealer_id" value="<?php echo esc_attr( get_option('aim_dealer_id') ); ?>" /></td>
+      </tr>
+    
+     <tr valign="top">
+      <th scope="row">AV SDK URL</th>
+      <td><input type="text" name="av_sdk_url" value="<?php echo esc_attr( get_option('av_sdk_url') ); ?>" /></td>
+      </tr>
+
+      <tr valign="top">
+      <th scope="row">AV Slack Webhook URL</th>
+      <td><input type="text" name="av_slack_url" value="<?php echo esc_attr( get_option('av_slack_url') ); ?>" /></td>
+      </tr>
+
+      <tr valign="top">
+      <th scope="row">AV Import Order</th>
+      <td><input type="text" name="av_import_order" value="<?php echo esc_attr( get_option('av_import_order') ); ?>" /></td>
       </tr>
 
       <tr valign="top">
@@ -77,34 +95,34 @@ function aim_plugin_settings_page() {
   echo ob_get_clean();
 }
 
-//content within the AIM shortcode tab
-function shortcode_aim() {
+//content within the av shortcode tab
+function shortcode_av() {
 
     ob_start();
     ?> 
   
   
   
-      <img src="<?php echo plugin_dir_url( dirname( __FILE__ ) ) . '/img/aim-experts-logo.png'; ?>">
+      <img src="<?php echo plugin_dir_url( dirname( __FILE__ ) ) . '/img/av-experts-logo.png'; ?>">
   
-      <h1>The easiest way to intergrate AIM with your WordPress site    </h1>
+      <h1>The easiest way to intergrate av with your WordPress site    </h1>
   
   
       <p>Just add some simple shortcode to your VDP and Listing pages<p>
       <p>Add this shortcode to your VDP page.  <p>
-          <pre>[aim-buttons-static]</pre>
+          <pre>[av-buttons-static]</pre>
   <p>If adding code to template file use the following code</p>
-  <pre>&lt;?php echo do_shortcode("[aim-buttons-static]"); ?&gt;</pre> 
+  <pre>&lt;?php echo do_shortcode("[av-buttons-static]"); ?&gt;</pre> 
   <p>for floating buttons add the following code to template file.</p>
-      <pre>&lt;?php echo do_shortcode("[aim-buttons-float]"); ?&gt;</pre> 
+      <pre>&lt;?php echo do_shortcode("[av-buttons-float]"); ?&gt;</pre> 
   <p>To add just VSA and calculator buttons to the vdp use the following short code</p>
-  <pre>[aim-buttons-vdp]</pre>
+  <pre>[av-buttons-vdp]</pre>
   <p>or use this code within the template file</p>
-      <pre>&lt;?php echo do_shortcode("[aim-buttons-vdp]"); ?&gt;</pre>
+      <pre>&lt;?php echo do_shortcode("[av-buttons-vdp]"); ?&gt;</pre>
   <p>Add this shortcode to your VLP page.  <p>
-  <pre>[aim-buttons-listing]</pre>
+  <pre>[av-buttons-listing]</pre>
   <p>If adding code to template file use the following code</p>
-  <pre>&lt;?php echo do_shortcode("[aim-buttons-listing]"); ?&gt;</pre>
+  <pre>&lt;?php echo do_shortcode("[av-buttons-listing]"); ?&gt;</pre>
                   
        <?php
     echo ob_get_clean();
